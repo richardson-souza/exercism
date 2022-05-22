6bd815f7435142b3944310414435d405d5586757
Richardson Allan Ferreira de Souza <richardson.allan.souza@gmail.com>
2022-05-21 23:31:09 -0300

    feature: define the 'preparation_time_in_minutes()' function

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -3,13 +3,10 @@
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
 EXPECTED_BAKE_TIME = 40
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
+PREPARATION_TIME = 2
 
 
-# TODO: define the 'bake_time_remaining()' function
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
@@ -24,8 +21,18 @@ def bake_time_remaining(elapsed_bake_time):
     return elapsed_bake_time - EXPECTED_BAKE_TIME
 
 
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
+def preparation_time_in_minutes(number_of_layers):
+    """Calculate how many minutes you would spend making the lasagna.
+
+    :param number_of_layers: int - number of layers you want to add to the lasagna.
+    :return: int * the time it takes to prepare a single layer from 'PREPARATION_TIME'.
+    
+    Function that takes the number of layers you want to add to the lasagna as
+    an argument and returns how many minutes you would spend making them.
+    Assume each layer takes 2 minutes to prepare.
+    """
+
+    return number_of_layers * PREPARATION_TIME
 
 
 # TODO: define the 'elapsed_time_in_minutes()' function
