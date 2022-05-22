5fb1fb480866ebf5431167f36dfa600a1e265f83
Richardson Allan Ferreira de Souza <richardson.allan.souza@gmail.com>
2022-05-22 00:52:10 -0300

    chore: 'Trailing whitespace' was reported by Pylint.

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -26,7 +26,7 @@ def preparation_time_in_minutes(number_of_layers):
 
     :param number_of_layers: int - number of layers you want to add to the lasagna.
     :return: int - time it takes to prepare a single layer from 'PREPARATION_TIME'.
-    
+
     Function that takes the number of layers you want to add to the lasagna as
     an argument and returns how many minutes you would spend making them.
     Assume each layer takes 2 minutes to prepare.
@@ -40,8 +40,9 @@ def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
 
     :param number_of_layers: int - number of layers you want to add to the lasagna.
     :return: int - the total elapsed minutes spent cooking the lasagna.
-    
-    This function takes two numbers representing the number of layers & the time already spent 
+
+    This function takes two numbers representing the number of layers & the time already spent
     baking and calculates the total elapsed minutes spent cooking the lasagna.
     """
+
     return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
