4872b51eb1a57614f005c3fe4ed6d874e77cdd08
Richardson Allan Ferreira de Souza <richardson.allan.souza@gmail.com>
2022-05-26 17:01:24 -0400

    chore: Trailing whitespace

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -13,7 +13,7 @@ def get_bold_text(text: str) -> str:
     """Identify if text has a valid character to be
     recognized as Markdown code for bold '__emphasized__'.
     :param text: str - Any text.
-    :return: str - Text with character recognized as 
+    :return: str - Text with character recognized as
     Markdown code to bold.
     """
     m = re.match(STR_BOLD, text)
@@ -24,7 +24,7 @@ def get_italic_text(text: str) -> str:
     """Identify if text has a valid character to be
     recognized as Markdown code for talicize ' _italicized_'.
     :param text: str - Any text.
-    :return: str - Text with character recognized as 
+    :return: str - Text with character recognized as
     Markdown code to talicize.
     """
     m = re.match(STR_ITALIC, text)
@@ -35,7 +35,7 @@ def get_list_text(text: str) -> str:
     """Identify if text has a valid character to be
     recognized as Markdown code for unordered lists '*'.
     :param text: str - Any text.
-    :return: str - Text with character recognized as 
+    :return: str - Text with character recognized as
     Markdown code to unordered lists.
     """
     m = re.match(STR_LIST, text)
@@ -43,16 +43,16 @@ def get_list_text(text: str) -> str:
         return m.group(1)
 
 def parsing_headers_level(text: str) -> str:
-    """Identify if text has a structurally valid Markdown 
+    """Identify if text has a structurally valid Markdown
     to recognizes up to 6 hash (#).
 
     :param text: str - Any text.
     :return: str - Text with valid HTML Section Heading elements.
 
-    Function that takes any text and converts it to a section 
-    header HTML elements if it identifies that text has a 
-    structurally valid Markdown to recognize up to 6 hash 
-    characters (#) for 6 levels. Otherwise returns text 
+    Function that takes any text and converts it to a section
+    header HTML elements if it identifies that text has a
+    structurally valid Markdown to recognize up to 6 hash
+    characters (#) for 6 levels. Otherwise returns text
     without changes.
     """
     headers = {}
@@ -62,7 +62,7 @@ def parsing_headers_level(text: str) -> str:
     for key in headers:
         if re.match(key, text) is not None:
             return headers[key]
-    
+
     return text
 
 def parsing_italic(match: re.Match) -> str:
@@ -110,7 +110,7 @@ def parse(markdown: str) -> str:
             i = '</ul>' + i
             in_list_append = False
         res += i
-    
+
     if in_list:
         res += '</ul>'
 
