561a9f692c6cb2088d24eeddc949595e23c0b931
Richardson Allan Ferreira de Souza <richardson.allan.souza@gmail.com>
2022-05-27 19:25:59 -0400

    refact: add parsing_paragraph and match_paragraph functions

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -8,6 +8,7 @@ from typing import Union
 STR_BOLD = r'(.*)__(.*)__(.*)'
 STR_ITALIC = r'(.*)_(.*)_(.*)'
 STR_LIST = r'\* (.*)'
+STR_PARAGRAPH = r'<h|<ul|<p|<li'
 
 
 def match_bold_text(text: str) -> Union[re.Match, None]:
@@ -36,6 +37,14 @@ def match_list_text(text: str) -> Union[re.Match, None]:
     :return: re.Match or None.
     """
     return re.match(STR_LIST, text)
+
+def match_paragraph(text: str) -> Union[re.Match, None]:
+    """Identify if text has a paragraph.
+
+    :param text: str - Any text.
+    :return: re.Match or None.
+    """
+    return re.match(STR_PARAGRAPH, text)
 
 def parsing_headers_level(text: str) -> str:
     """Identify if text has a structurally valid Markdown
@@ -78,7 +87,10 @@ def parsing_bold(match: re.Match) -> str:
     return f'{match.group(1)}<strong>{match.group(2)}</strong>{match.group(3)}'
 
 def parsing_bold_italic(text: str) -> str:
-    """
+    """Convert text with Markdown code for bold or talicize in a valid HTML <strong>.
+
+    :param text: str - Any text.
+    :return: str - Text with strong importance element and/or  emphasis element.
     """
     bold_text = match_bold_text(text)
     if bold_text:
@@ -96,9 +108,22 @@ def parsing_list(text: str, in_list: bool) -> Union[str, bool]:
     :param text: str - Literal with valid Mardown code '*'.
     :return: str - Text with unordered list element <ul><li>item</li></ul>.
     """
+    if match_bold_text(text) is not None or match_italic_text(text) is not None:
+        text = parsing_bold_italic(text)
+
     if not in_list:
         return '<ul><li>' + text + '</li>', True
     return '<li>' + text + '</li>', in_list
+
+def parsing_paragraph(text: str) -> str:
+    """Add valid HTML <p>.
+
+    :param text: str - Literal with valid Mardown code.
+    :return: str - Text with paragraph element <p>paragraph</p>.
+    """
+    if not match_paragraph(text):
+        return '<p>' + text + '</p>'
+    return text
 
 def parse(markdown: str) -> str:
     """Main function
@@ -110,20 +135,19 @@ def parse(markdown: str) -> str:
         i = parsing_headers_level(i)
         match = match_list_text(i)
         if match:
-            text = parsing_bold_italic(match.group(1))
-            i, in_list = parsing_list(text, in_list)
+            i, in_list = parsing_list(match.group(1), in_list)
         else:
             if in_list:
                 in_list_append = True
                 in_list = False
 
-        match = re.match('<h|<ul|<p|<li', i)
-        if not match:
-            i = '<p>' + i + '</p>'
+        i = parsing_paragraph(i)
         i = parsing_bold_italic(i)
+
         if in_list_append:
             i = '</ul>' + i
             in_list_append = False
+
         res += i
 
     if in_list:
